5bf7f5fc680db57e88bd1b2851caa9c09ff05e6b
suxutao <113399058+suxutao@users.noreply.github.com>
2024-11-12 14:34:24 +0800

    欢迎界面业务逻辑完成

--- app/src/main/java/com/example/tinychatbox/controller/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/tinychatbox/controller/activity/WelcomeActivity.java
@@ -1,17 +1,58 @@
 package com.example.tinychatbox.controller.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tinychatbox.R;
+import com.hyphenate.chat.EMClient;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler=new Handler(){
+        public void handleMessage(@NonNull Message msg){
+            if (isFinishing()){
+                return;
+            }
+            //判断进入主页面还是登录页面
+            toMainOrLogin();
+        }
+
+        private void toMainOrLogin() {
+            new Thread(){
+                public void run(){
+                    if (EMClient.getInstance().isLoggedInBefore()){
+
+                        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }.start();
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_welcome);
+        //两秒钟延时
+        handler.sendMessageDelayed(Message.obtain(),2000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
